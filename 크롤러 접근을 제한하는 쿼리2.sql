11e28df78851dedcf246e6aa47456e73e8e33ba8
songi061 <104684923+songi061@users.noreply.github.com>
2022-05-27 15:30:55 +0900

    Create 크롤러 접근을 제한하는 쿼리2.sql

---
+++ 크롤러 접근을 제한하는 쿼리2.sql
@@ -0,0 +1,28 @@
+--<마스터 데이터를 사용해 제외하는 쿼리>
+with
+mst_bot_user_agent as(
+             select '%bot%'      as rule
+  union all  select '%crawler%'  as rule
+  union all  select '%spider%'   as rule
+  union all  select '%archiver%' as rule
+)
+, filtered_action_log as(
+  select
+    l.stamp, l.session, l.action, l.products
+    , l.url , l.ip, l.user_agent
+  --UserAgent의 규칙에 해당하지 않는 로그만 남기기 => Where 구문에 상관 서브쿼리를 사용할 수 있음
+  from
+  `sql-study-349216.ch7.action_log_with_noise` as l
+  where
+   not exists (
+     select 1
+     from mst_bot_user_agent as m   
+     where
+     l.user_agent like m.rule 
+   )
+)  
+select
+ *
+from
+ filtered_action_log
+;
